8008c3980f7311f65d3f678d6994a01c692e5445
SunKang <coolshark248@gmail.com>
2024-06-19 13:21:55 -0600

    ready for coding

---
+++ DeepLinkPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DeepLinkPractice
+//
+//  Created by 강성찬 on 2024-06-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DeepLinkPractice/DeepLinkPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeepLinkPracticeApp.swift
+//  DeepLinkPractice
+//
+//  Created by 강성찬 on 2024-06-19.
+//
+
+import SwiftUI
+
+@main
+struct DeepLinkPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
